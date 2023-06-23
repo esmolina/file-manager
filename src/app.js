0833506e98984ff6e544b3621895dd5ef2e2bab8
esmolina <elenasmolinann@gmail.com>
2023-06-23 12:13:53 +0300

    feat: add exit by Ctrl+C and .exit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,30 @@
-import { stdout } from 'node:process'
+import { stdin, stdout } from 'node:process'
 import { getRelativeDirname } from "./utils/getDirname.js";
 import { getUserName} from "./utils/getUserName.js";
 import {getArgs} from "./utils/getArgs.js";
 
+const inputChannel = stdin;
 const outputChannel = stdout;
 const __dirname = getRelativeDirname(import.meta.url);
 const enteredArgs = getArgs();
 const userName = getUserName(enteredArgs);
 
 const startApp = () => {
-  outputChannel.write(`Welcome to the File Manager, ${userName}!\n`);
+  outputChannel.write(`Welcome to the File Manager, ${userName}!\n\n`);
+  inputChannel.on('data', enteredSymbols => {
+    const userCommand = (`${enteredSymbols}`).trim().toLowerCase();
+    // Exit by .exit
+    if (userCommand === '.exit') {
+      outputChannel.write(`\nThank you for using File Manager, ${userName}, goodbye!\n`);
+      process.exit(0);
+    }
+  })
+
+  // Exit by CTRL+C
+  process.on('SIGINT', () => {
+    outputChannel.write(`\nThank you for using File Manager, ${userName}, goodbye!\n`);
+    process.exit(0);
+  });
 }
 
 startApp();
