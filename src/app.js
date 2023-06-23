f6de694e6539fe3c56a328113b9bd66fdb43c60e
esmolina <elenasmolinann@gmail.com>
2023-06-23 09:50:14 +0300

    feat: start app with greating

--- src/app.js
+++ src/app.js
@@ -1,12 +1,15 @@
+import { stdout } from 'node:process'
 import { getRelativeDirname } from "./utils/getDirname.js";
 import { getUserName} from "./utils/getUserName.js";
 import {getArgs} from "./utils/getArgs.js";
 
+const outputChannel = stdout;
 const __dirname = getRelativeDirname(import.meta.url);
 const enteredArgs = getArgs();
+const userName = getUserName(enteredArgs);
 
 const startApp = () => {
-  console.log(getUserName(enteredArgs));
+  outputChannel.write(`Welcome to the File Manager, ${userName}!\n`);
 }
 
 startApp();
