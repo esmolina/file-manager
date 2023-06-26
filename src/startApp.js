e651d33938a7d3f589e48f251a56fbf344f6fc9a
esmolina <elenasmolinann@gmail.com>
2023-06-26 14:09:35 +0300

    refactor: remove unused code

--- src/startApp.js
+++ src/startApp.js
@@ -1,7 +1,5 @@
 import { stdin, stdout } from 'node:process';
 import { createInterface } from 'node:readline/promises';
-import { getRelativeDirname } from "./utils/getDirname.js";
-import { getWorkingDirectoryPath } from "./utils/getWorkingDirectoryPath.js";
 import { sayHi, sayGoodbye } from "./utils/sayHiGoodbye.js";
 import { executeUsersCommand } from "./utils/executeUsersCommand.js";
 import { parseReadlineInput } from "./utils/parseReadlineInput.js";
@@ -12,10 +10,6 @@ export const readline = createInterface({
   input: stdin,
   output: stdout
 })
-
-// const inputChannel = stdin;
-const __dirname = getRelativeDirname(import.meta.url);
-const currentDirectory = getWorkingDirectoryPath();
 
 async function startApp () {
   sayHi();
