1f8205c8ffb9c149faddbdda9e2ffb58cee3fcbc
esmolina <elenasmolinann@gmail.com>
2023-06-26 14:51:26 +0300

    refactor: rewrite cd

--- src/userCommands/cd.js
+++ src/userCommands/cd.js
@@ -1,12 +1,12 @@
 import { join } from 'node:path';
-import { access } from 'node:fs/promises';
+import { access, stat } from 'node:fs/promises';
 import { sep } from 'node:path';
 import { stateStorage } from "../storage.js";
 import { fixEnteredPathsEnd } from "../utils/fixEnteredPathsEnd.js";
 import { checkIsRelativePath } from "../utils/checkIsRelativePath.js";
 
 const errorMessage = '\nOperation failed';
-const errorTypeMessage = 'There is no such directory';
+const errorTypeMessage = 'There is no such folder';
 
 export async function executeCd () {
   const path = stateStorage.currentArgs.join('');
@@ -17,8 +17,13 @@ export async function executeCd () {
   const absolutePath = isRelativePath? join(currentPath.toString(), fixedPath) : fixedPath;
 
   try {
-    await access(absolutePath);
-    stateStorage.currentUserPath = absolutePath;
+    const pathStat = await stat(absolutePath);
+    if (pathStat.isDirectory()) {
+      stateStorage.currentUserPath = absolutePath;
+    } else {
+      console.log(errorMessage);
+      console.log(errorTypeMessage);
+    }
   } catch (err) {
     console.log(errorMessage);
     console.log(errorTypeMessage);
