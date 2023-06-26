6840916931ed3250a37587472df447604134f427
esmolina <elenasmolinann@gmail.com>
2023-06-26 14:57:12 +0300

    refactor: make getting an absolute path into a separate function

--- src/userCommands/cd.js
+++ src/userCommands/cd.js
@@ -1,21 +1,12 @@
-import { join } from 'node:path';
-import { access, stat } from 'node:fs/promises';
-import { sep } from 'node:path';
+import { stat } from 'node:fs/promises';
 import { stateStorage } from "../storage.js";
-import { fixEnteredPathsEnd } from "../utils/fixEnteredPathsEnd.js";
-import { checkIsRelativePath } from "../utils/checkIsRelativePath.js";
+import {getAbsolutePath} from "../utils/getAbsolutePath.js";
 
 const errorMessage = '\nOperation failed';
 const errorTypeMessage = 'There is no such folder';
 
 export async function executeCd () {
-  const path = stateStorage.currentArgs.join('');
-  const currentPath = stateStorage.currentUserPath.toString();
-  //if the user has put an extra slash at the end of the line
-  const fixedPath = fixEnteredPathsEnd(path, sep);
-  const isRelativePath = checkIsRelativePath(fixedPath);
-  const absolutePath = isRelativePath? join(currentPath.toString(), fixedPath) : fixedPath;
-
+  const absolutePath = getAbsolutePath();
   try {
     const pathStat = await stat(absolutePath);
     if (pathStat.isDirectory()) {
