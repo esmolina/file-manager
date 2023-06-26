482ae1ead3e9891ec3d128bfbcf4cc31bb7a0ec4
esmolina <elenasmolinann@gmail.com>
2023-06-27 02:00:35 +0300

    fix: rewrite cat with read stream

--- src/userCommands/cat.js
+++ src/userCommands/cat.js
@@ -1,25 +1,41 @@
-import { stat, readFile } from 'node:fs/promises';
+import { stat } from 'node:fs/promises';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process'
 import {getAbsolutePath} from "../utils/getAbsolutePath.js";
-import {readline} from "../startApp.js";
 import {stateStorage} from "../storage.js";
 
+const outputChannel = stdout;
 const errorMessage = `\nOperation failed`;
 const errorTypeMessage = 'There is no such file';
 
-export async function executeCat () {
-  const path = stateStorage.currentArgs.join('');
-  const absolutePath = getAbsolutePath(path);
+export async function executeCat() {
+  const filePath = stateStorage.currentArgs.join('');
+  const absolutePath = getAbsolutePath(filePath);
+
   try {
-    const pathStat = await stat(absolutePath);
-    if (pathStat.isFile()) {
-      const fileContent = await readFile(absolutePath);
-      readline.setPrompt(fileContent);
-      readline.prompt();
+    const fileStat = await stat(absolutePath);
+
+    if (fileStat.isFile()) {
+      const readStream = createReadStream(absolutePath);
+
+      readStream.on('data', (fileContent) => {
+        outputChannel.write(`${fileContent}\n`);
+      });
+
+      readStream.on('error', (error) => {
+        outputChannel.write(errorMessage);
+      });
+
+      await new Promise((resolve) => {
+        readStream.on('close', () => {
+          resolve();
+        });
+      });
     } else {
       console.log(errorMessage);
       console.log(errorTypeMessage);
     }
-  } catch (err) {
+  } catch (error) {
     console.log(errorMessage);
     console.log(errorTypeMessage);
   }
